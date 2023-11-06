a09a97790ac8a33c7e67c169e2cd7f20e81da9ec
MrDrDude <mr.dr.kali.linux@gmail.com>
2023-11-06 10:33:55 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    a = 5
+    b = 7
+
+    # Установите точку останова (breakpoint) на следующей строке.
+    breakpoint()
+
+    # Измените значения переменных.
+    a += 3
+    b -= 2
+
+    result = a + b
+    print(f"Результат: {result}")
+
+if __name__ == "__main__":
+    main()
